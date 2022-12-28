472981d52fb3cdd4f2c52cce65a5c2e02d751b60
aldaniyazov <alda.kanat@gmail.com>
2022-12-28 10:19:02 +0600

    added a solution to the problem by arrays

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter any elements of the array separated by a space");
+string s = Console.ReadLine();
+string[] line = s.Split(' ');
+string[] array = new string[line.Length];
+
+void ThreeCharElements(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = line[i];
+        if (array[i].Length <= 3)
+            Console.Write($"{array[i]} ");
+    }
+}
+ThreeCharElements(array);
